885fa3a56464c01ee43ded241a278c4c4e1179f6
Robert Crocker <robcrock@Roberts-Laptop.local>
2023-08-12 13:58:27 -0400

    Add dynamic blog content.

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,16 @@ import BlogSummaryCard from "@/components/BlogSummaryCard"
 
 import styles from "./homepage.module.css"
 
-import { getBlogPostList } from "@/helpers/file-helpers.js"
+import { getBlogPostList } from "@/helpers/file-helpers"
 
 async function Home() {
-  const posts = await getBlogPostList()
+  const blogPosts = await getBlogPostList()
 
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.mainHeading}>Latest Content:</h1>
       {/* TODO: Iterate over the data read from the file system! */}
-      {posts.map(({ slug, title, abstract, publishedOn }) => {
+      {blogPosts.map(({ slug, title, abstract, publishedOn }) => {
         return (
           <BlogSummaryCard
             key={slug}
