4fa6dea02ccdc1f4874fe35e3a1cba99a5622cf0
Robert Crocker <robcrock@Roberts-Laptop.local>
2023-08-18 22:29:26 -0400

    Toggle Pause and Play buttons.

--- src/components/CircularColorsDemo/CircularColorsDemo.js
+++ src/components/CircularColorsDemo/CircularColorsDemo.js
@@ -1,65 +1,51 @@
-import React from 'react';
-import clsx from 'clsx';
-import {
-  Play,
-  Pause,
-  RotateCcw,
-} from 'react-feather';
+"use client"
 
-import Card from '@/components/Card';
-import VisuallyHidden from '@/components/VisuallyHidden';
+import React, { useState } from "react"
+import clsx from "clsx"
+import { Play, Pause, RotateCcw } from "react-feather"
 
-import styles from './CircularColorsDemo.module.css';
+import Card from "@/components/Card"
+import VisuallyHidden from "@/components/VisuallyHidden"
+
+import styles from "./CircularColorsDemo.module.css"
 
 const COLORS = [
-  { label: 'red', value: 'hsl(348deg 100% 60%)' },
-  { label: 'yellow', value: 'hsl(50deg 100% 55%)' },
-  { label: 'blue', value: 'hsl(235deg 100% 65%)' },
-];
+  { label: "red", value: "hsl(348deg 100% 60%)" },
+  { label: "yellow", value: "hsl(50deg 100% 55%)" },
+  { label: "blue", value: "hsl(235deg 100% 65%)" },
+]
 
 function CircularColorsDemo() {
+  const [isPlaying, setIsPlaying] = useState(false)
   // TODO: This value should increase by 1 every second:
-  const timeElapsed = 0;
+  const timeElapsed = 0
 
   // TODO: This value should cycle through the colors in the
   // COLORS array:
-  const selectedColor = COLORS[0];
+  const selectedColor = COLORS[0]
 
   return (
     <Card as="section" className={styles.wrapper}>
       <ul className={styles.colorsWrapper}>
         {COLORS.map((color, index) => {
-          const isSelected =
-            color.value === selectedColor.value;
+          const isSelected = color.value === selectedColor.value
 
           return (
-            <li
-              className={styles.color}
-              key={index}
-            >
-              {isSelected && (
-                <div
-                  className={
-                    styles.selectedColorOutline
-                  }
-                />
-              )}
+            <li className={styles.color} key={index}>
+              {isSelected && <div className={styles.selectedColorOutline} />}
               <div
                 className={clsx(
                   styles.colorBox,
-                  isSelected &&
-                    styles.selectedColorBox
+                  isSelected && styles.selectedColorBox
                 )}
                 style={{
                   backgroundColor: color.value,
                 }}
               >
-                <VisuallyHidden>
-                  {color.label}
-                </VisuallyHidden>
+                <VisuallyHidden>{color.label}</VisuallyHidden>
               </div>
             </li>
-          );
+          )
         })}
       </ul>
 
@@ -69,18 +55,18 @@ function CircularColorsDemo() {
           <dd>{timeElapsed}</dd>
         </dl>
         <div className={styles.actions}>
-          <button>
-            <Play />
+          <button onClick={() => setIsPlaying(!isPlaying)}>
+            {isPlaying ? <Pause /> : <Play />}
             <VisuallyHidden>Play</VisuallyHidden>
           </button>
-          <button>
+          <button onClick={() => setIsPlaying(false)}>
             <RotateCcw />
             <VisuallyHidden>Reset</VisuallyHidden>
           </button>
         </div>
       </div>
     </Card>
-  );
+  )
 }
 
-export default CircularColorsDemo;
+export default CircularColorsDemo
--- src/helpers/mdx-components.js
+++ src/helpers/mdx-components.js
@@ -1,7 +1,9 @@
 import CodeSnippet from "../components/CodeSnippet/CodeSnippet"
 import DivisionGroupsDemo from "../components/DivisionGroupsDemo/DivisionGroupsDemo"
+import CircularColorsDemo from "../components/CircularColorsDemo/CircularColorsDemo"
 
 export const COMPONENT_MAP = {
   pre: CodeSnippet,
   DivisionGroupsDemo,
+  CircularColorsDemo,
 }
