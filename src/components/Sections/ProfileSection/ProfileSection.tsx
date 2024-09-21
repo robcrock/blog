5c5811dd9e96cc6e13998d3b3563e307bb60d9df
Robert Crocker <rcrocker13@gmail.com>
2024-09-21 18:20:48 -0500

    Added avatar.

--- src/components/Sections/ProfileSection/ProfileSection.tsx
+++ src/components/Sections/ProfileSection/ProfileSection.tsx
@@ -1,20 +1,15 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Github, Linkedin, Link as LinkIcon, X } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
-
-import avatar from "../../../../public/images/avatar.png";
 
 export default function ProfileSection() {
   return (
     <section className="flex gap-10 mt-10 mb-20">
-      <Image
-        src={avatar}
-        alt="Robert Crocker"
-        width={120}
-        height={120}
-        className="border-2 rounded-full"
-      />
+      <Avatar className="w-[120px] h-[120px] border-2">
+        <AvatarImage src="/images/avatar.png" alt="Robert Crocker" />
+        <AvatarFallback>RC</AvatarFallback>
+      </Avatar>
       <div className="flex flex-col items-start justify-center gap-2">
         <div className="flex flex-col items-start justify-center">
           <h1 className="text-3xl font-bold">Robert Crocker</h1>
