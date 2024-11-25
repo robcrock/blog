6044f016fb4d1fe8a5694a30659c8638fa29af6d
Robert Crocker <rcrocker13@gmail.com>
2024-11-24 23:18:16 -0600

    Fixed darkmode text and logo.

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -6,8 +6,7 @@ const PostContent = ({ children, ...props }: PostContentProps) => {
       {...props}
       className={`
         lg:w-content-with-sidebar lg:pr-12 mb-12
-        
-        [&_p]:text-lg md:[&_p]:text-xl md:[&_p]:leading-8 [&_p]:px-2 [&_p]:my-3 text-gray-800
+        [&_p]:text-lg md:[&_p]:text-xl md:[&_p]:leading-8 [&_p]:px-2 [&_p]:my-3 
       `}
     >
       {children}
--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,4 +1,16 @@
 export function Logo(props) {
+  // Check if we're in dark mode by looking for the 'dark' class on html element
+  const isDark =
+    typeof document !== "undefined"
+      ? document.documentElement.classList.contains("dark")
+      : false;
+
+  // Show light logo (white) in dark mode, dark logo (black) in light mode
+  return isDark ? <LightLogo {...props} /> : <DarkLogo {...props} />;
+}
+export default Logo;
+
+const LightLogo = (props) => {
   return (
     <svg
       width={49}
@@ -56,6 +68,64 @@ export function Logo(props) {
       </defs>
     </svg>
   );
-}
+};
 
-export default Logo;
+const DarkLogo = (props) => {
+  return (
+    <svg
+      width={48}
+      height={46}
+      viewBox="0 0 48 46"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M8 5.069l30.615 36.355a1 1 0 01-.764 1.645H8v-38z"
+        fill="#fff"
+        fillOpacity={0.6}
+      />
+      <g filter="url(#filter0_d_353_853)">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M26.02 5H8v31h18.02v-.008C34.34 35.718 41 28.894 41 20.517c0-8.377-6.66-15.2-14.98-15.474V5z"
+          fill="#fff"
+          fillOpacity={0.6}
+          shapeRendering="crispEdges"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_353_853"
+          x={0}
+          y={-1}
+          width={49}
+          height={47.0001}
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy={2} />
+          <feGaussianBlur stdDeviation={4} />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix values="0 0 0 0 0.984912 0 0 0 0 0.984912 0 0 0 0 0.984912 0 0 0 0.25 0" />
+          <feBlend
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_353_853"
+          />
+          <feBlend
+            in="SourceGraphic"
+            in2="effect1_dropShadow_353_853"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
