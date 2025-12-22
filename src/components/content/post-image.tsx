6d23c38073185a82d4f4513fcefc1bdea413b0dd
robcrock <rcrocker13@gmail.com>
2025-12-21 20:29:03 -0500

    Make PostImage responsive and a11y.

--- src/components/content/post-image.tsx
+++ src/components/content/post-image.tsx
@@ -3,15 +3,20 @@
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import Image from "next/image";
 
-export default function PostImage({ src }: { src: string }) {
+interface PostImageProps {
+  src: string;
+  alt: string;
+}
+
+export default function PostImage({ src, alt }: PostImageProps) {
   return (
-    <div className="w-[450px] my-10 mx-auto">
+    <div className="w-full max-w-[450px] sm:max-w-[600px] lg:max-w-[800px] my-10 mx-auto px-4 sm:px-0">
       <AspectRatio ratio={16 / 9}>
         <Image
-          width={325}
-          height={253}
+          width={800}
+          height={450}
           src={src}
-          alt="Image"
+          alt={alt}
           className="object-cover rounded-md"
         />
       </AspectRatio>
